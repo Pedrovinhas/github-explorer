c411a16a7ae5aa43b967fcb923d23871858185c5
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-04-12 15:27:57 -0300

    Chamadas HTTP :sparkles:

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,24 @@
+import { useState, useEffect } from "react"
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss'
+
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect( () => {
+        fetch('https://api.github.com/users/pedrovinhas/repos')
+        .then(response => response.json())
+        .then(data=> setRepositories(data))
+    }, [repositories]);
+
     return (
         <section className="repository-list">
             <h1> Lista de repositórios</h1>
             <ul> 
-            <RepositoryItem />
-            <RepositoryItem />
-            <RepositoryItem />
-            <RepositoryItem />
+            {repositories.map(repository => {
+                return <RepositoryItem key={repository.name} repository={repository} />
+            })}
             </ul>
         </section>
     )

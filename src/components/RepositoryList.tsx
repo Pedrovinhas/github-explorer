1c039252b28758644a7f974ae7a5995145b8ba74
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2021-04-12 22:44:20 -0300

    Usando TypeScript :pencil2:

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect( () => {
         fetch('https://api.github.com/users/pedrovinhas/repos')
@@ -22,4 +28,4 @@ export function RepositoryList() {
             </ul>
         </section>
     )
-} 
+}  
